6cd43606768cf14d2c005b0b0d5073d4bf7c46a9
James Cape <james@mobilecoin.com>
2023-02-22 11:38:55 -0800

    Add a basic rendering test.

---
+++ render-tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+
+rundir=$(dirname $0)
+cookiecutter_directory="$1"
+repository_root="$(git rev-parse --show-toplevel)"
+
+for test in $(find "${cookiecutter_directory}/tests" -maxdepth 1 -mindepth 1 -type d); do
+	rm --recursive --force -- "${test}/output"
+	cookiecutter \
+		--no-input \
+		--config-file "${test}/cookiecutter.yaml" \
+		--output-dir "${test}/output" \
+		--directory "${cookiecutter_directory}" \
+		--overwrite-if-exists \
+		"${repository_root}"
+done
