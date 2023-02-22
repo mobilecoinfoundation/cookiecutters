e7242653293d600ca4a13ca8035b98fc724f56b0
James Cape <james@mobilecoin.com>
2023-02-22 02:09:36 -0800

    Create github_username extension

---
+++ rust/repo/local_extensions.py
@@ -0,0 +1,21 @@
+from jinja2.ext import Extension
+import subprocess
+
+
+class GithubUsernameExtension(Extension):
+    """Jinja2 Extension to get the current Github username via `gh`.
+    """
+
+    def __init__(self, environment):
+        super().__init__(environment)
+
+        def github_username():
+            output = subprocess.run(["gh", "api", "user"], capture_output=True)
+            if output.returncode != 0:
+                return ""
+
+            user = json.loads(output.stdout)
+            return user["login"]
+
+        environment.globals.update(github_username=github_username)
+
