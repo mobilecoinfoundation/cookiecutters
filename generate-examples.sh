228091de148a6763455a8c2a1bdc575bc49d89d0
James Cape <jamescape777@gmail.com>
2023-03-03 15:28:24 -0800

    Rename tests to examples (#39)
    * Rename tests to examples
    
    * Use poetry in generate-examples.sh.
    
    * Adjust CI for script rename.
    
    * Make poetry use more better
    
    * Address labeler comments.
    
    * Render labeler changes.

--- generate-examples.sh
+++ generate-examples.sh
@@ -6,9 +6,11 @@ rundir=$(dirname $0)
 cookiecutter_directory="$1"
 repository_root="$(git rev-parse --show-toplevel)"
 
-for test in $(find "${cookiecutter_directory}/tests" -maxdepth 1 -mindepth 1 -type d); do
+poetry install
+
+for test in $(find "${cookiecutter_directory}/examples" -maxdepth 1 -mindepth 1 -type d); do
 	rm --recursive --force -- "${test}/output"
-	cookiecutter \
+	poetry run cookiecutter \
 		--no-input \
 		--config-file "${test}/cookiecutter.yaml" \
 		--output-dir "${test}/output" \
