7e02b5aafdb806a6e2055a7fe03816ae791bb42a
James Cape <james@mobilecoin.com>
2023-03-03 07:54:30 -0800

    Rename tests to examples

--- generate-examples.sh
+++ generate-examples.sh
@@ -6,7 +6,7 @@ rundir=$(dirname $0)
 cookiecutter_directory="$1"
 repository_root="$(git rev-parse --show-toplevel)"
 
-for test in $(find "${cookiecutter_directory}/tests" -maxdepth 1 -mindepth 1 -type d); do
+for test in $(find "${cookiecutter_directory}/examples" -maxdepth 1 -mindepth 1 -type d); do
 	rm --recursive --force -- "${test}/output"
 	cookiecutter \
 		--no-input \
